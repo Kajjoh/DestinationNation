13c4e2ab5b154d03d2ef7faa4b9cbeae48a9db3b
Marina Bengtsson <bengtsson.marina@hotmail.com>
2018-05-15 17:00:48 +0200

    Ändrat html filen till php fil där jag också kopplat till databasen. Jag har bytt namn på den första php filen till sparaAnmalan då det blir tydligare vad den gör.

--- AnmalanEv.php
+++ AnmalanEv.php
@@ -9,44 +9,143 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error){
     die("Connection failed: " . $conn->connect_error);
 };
-
-/*koden ska spara in alla anmälningar på databasen. När en användare har skrivit 
-sin anmälan kommer den till den ny sida med "tack för din anmälan" och kan
-sedan skickas tillbaka till startsidan.*/
-
-//Skapar variabelnamn för alla fält
-
-$person = mysqli_real_escape_string($conn, trim($_POST['Namn']));
-$email = mysqli_real_escape_string($conn, trim($_POST['Mail']));
-$kon = ($_POST['kon']);
-$matpref = ($_POST['mat']);
-$drickpref = ($_POST['dryck']);
-$allergi = ($_POST['Allergi']);
-$kompis = ($_POST['Sallskap']);
-/*$personErr = "";
-$emailErr = "";*/
-
-//Tar hand om fallen för fält som inte får vara tomma. 
-
-if(empty($person)){
-    echo 'OBS! Fyll i ditt för- och efternamn.'
-} 
-
-if(empty($email) || preg_match("/^[a-zA-Z0-9_.-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$/", $email)){
-    //$emailErr = "OBS! Obligatoriskt fält.";
-    echo 'OBS! Fyll i en giltig e-postadress.';
-    die();
-}
-
-}
-else{
-    $sql = "INSERT INTO anmalan (namn, mail, kon, mat, allergi, dryck, sallskap)
-    VALUES ('$person', '$email', '$kon', '$matpref', '$allergi', '$drickpref', '$kompis')";
-    $conn->query($sql);
-    echo 'Tack för din anmälan!'
-}
-
-$conn->close();
 ?>
 
+<DOCTYPE html>
+<html>
+	
+	<head>
+		<title>AnmalanEv</title>
+		<meta charset="utf-8">
+		<link href="main.css" rel="stylesheet">
+	</head>
+	
+	<body>
 
+		<!-- Det här är sidhuvudet, det ska ligga högst upp på varje sida. --> 
+		<div id="sidhuvud">
+        	<img src="images/headbackground.jpg" alt="Red board" width=100% height="40">
+    	</div>
+
+		<div id="header">
+			<img src="images\logotest.jpg" class="logo"> 
+    	</div>
+
+		<!-- I den här menyn väljer man vart man ska. Ska ligga högst upp på majoriteten av sidorna. -->
+    	<div class="huvudMeny">
+			<ul>
+			  <li id="listaMeny"><a href="startsidaStudent.html">Start</a></li>
+			  <li style="float:right" id="listaMeny"><a href="inloggning.html">Logga ut</a></li>
+			  <li style="float:right"id="listaMeny"><a href="minaSidorStudent.html">Mina sidor</a></li>
+			  <li style="float:right" id="listaMeny"><a href="OmOss.html">Om Oss</a></li>
+			</ul>
+		</div>
+
+		<!-- Här börjar innehållssidan. --> 
+
+		<div class="divAnmalan">
+
+			<div id="rubrikAnmalan">
+				<h2>Anmälan till "Gasque"</h2>
+			</div>
+
+			<!-- Här följer ett formulär för anmälan till evenemang. --> 
+			<div class="formAnmalan">
+				<form id="AnmalanEv" method="Post" action="sparaAnmalan.php">
+				
+					<div class="formTop">
+					
+						<p class="boxes"></p>
+							<label for="Namn">Namn</label><br>
+								<input type="text" id="Namn"><br>
+						</p>
+				
+						
+						<label for="Mail">E-post</label><br>
+							<input type="text" id="Mail"><br>
+						
+				
+						<p class="boxes"></p>
+							<label for="Kön">Kön</label><br>
+							<select name="kon">
+							  <option value="Man">Man</option>
+							  <option value="Kvinna">Kvinna</option>
+							</select>
+						</p>
+
+						<p class="boxes">
+							<label for="Sallskap">Sällskap</label><br>
+								<input type="text" id="Sallskap"><br>
+						</p>
+					</div>
+					
+					<div class="formLeft">
+						
+						<p class="boxes">
+							<label for="Mat">Matpreferens</label><br>
+							<select name="mat">
+							  <option value="Inga">Inga preferenser</option>
+							  <option value="Vegi">Vegetariskt</option>
+							  <option value="Veg">Vegan</option>
+							</select>
+											
+						</p>
+					</div>
+
+					<div class="formRight">
+						<p class="boxes">
+							<label for="Dryck">Måltidsdryck</label><br>
+							<select name="dryck">
+							  <option value="alko">Alkoholfritt</option>
+							  <option value="vin">Vin till maten</option>
+							  <option value="cider">Cider & Vin</option>
+							  <option value="ol">Öl & Vin</option>
+							</select>
+						</p>
+					</div>
+
+					<div class="formBottom">
+
+						<div id="formAllergi"> 
+							<label for="Allegri">Allergi</label></br>
+								<textarea placeholder="Skriv in eventuella allergier" id="Allergi"></textarea>
+						</div>
+						
+						<div id="anmalanSkicka">
+							<input type="submit" value="Skicka" id="submit">
+						</div>
+							
+					</div>
+				
+				</form>
+			</div>
+		</div> 
+
+
+		<!-- Här följer sidfoten som ska ligga på alla sidor. --> 
+		<div id="sidfot">
+        	<div class="nationsloggor">
+            	<img src="images/logo-gh.jpg" alt="gh" width="100" height="100">
+            	<img src="images/logo-goteborgs.jpg" alt="gote" width="75" height="85">
+            	<img src="images/logo-gotlands.jpg" alt="got" width="90" height="90">
+            	<img src="images/logo-norrlands.jpg" alt="norr" width="90" height="90">
+            	<img src="images/logo-og.jpg" alt="og" width="85" height="90">
+            	<img src="images/logo-smalands.jpg" alt="sma" width="100" height="90">
+            	<img src="images/logo-snerikes.jpg" alt="sner" width="90" height="100">
+            	<img src="images/logo-stockholms.jpg" alt="stock" width="90" height="90">
+            	<img src="images/logo-uplands.jpg" alt="up" width="90" height="90">
+            	<img src="images/logo-varmlands.jpg" alt="varm" width="90" height="90">
+            	<img src="images/logo-vdala.jpg" alt="vd" width="90" height="90">
+            	<img src="images/logo-vg.jpg" alt="vg" width="70" height="90">
+            	<img src="images/logo-kal.png" alt="Kalmar" width="80" height="90">
+        	</div>
+
+        	<div id="footer">
+            	<img src="images/headbackground.jpg" alt="Red board" width=100% height="20">
+        	</div>
+		</div>
+		
+		<script src="AnmalanEv.js" ></script>
+
+	</body>
+</html>
