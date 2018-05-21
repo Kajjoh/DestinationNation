8cd7de3ba13a29fa0d5828bc04e9e57cd32af9c5
kajsajohansson <kajssajohansson@gmail.com>
2018-05-21 08:55:50 +0200

    Fixat sidfot

--- startsidaStudent.php
+++ startsidaStudent.php
@@ -10,7 +10,7 @@
     <body>
 
     <?php
-        include('sidhuvud.php');
+        include('sidhuvud.html');
     ?>
         
         <!--Detta är menyn där man kan logga ut, gå till mina sidor etc.-->
@@ -58,29 +58,9 @@
             <a id="nationsattr" href="#">Östgöta nation</a>
         </div>
 
-    
-        <!-- Här följer sidfoten som ska ligga på alla sidor. --> 
-        <div id="sidfot">
-            <div class="nationsloggor">
-                <img src="images/logo-gh.jpg" alt="gh" width="100" height="100">
-                <img src="images/logo-goteborgs.jpg" alt="gote" width="75" height="85">
-                <img src="images/logo-gotlands.jpg" alt="got" width="90" height="90">
-                <img src="images/logo-norrlands.jpg" alt="norr" width="90" height="90">
-                <img src="images/logo-og.jpg" alt="og" width="85" height="90">
-                <img src="images/logo-smalands.jpg" alt="sma" width="100" height="90">
-                <img src="images/logo-snerikes.jpg" alt="sner" width="90" height="100">
-                <img src="images/logo-stockholms.jpg" alt="stock" width="90" height="90">
-                <img src="images/logo-uplands.jpg" alt="up" width="90" height="90">
-                <img src="images/logo-varmlands.jpg" alt="varm" width="90" height="90">
-                <img src="images/logo-vdala.jpg" alt="vd" width="90" height="90">
-                <img src="images/logo-vg.jpg" alt="vg" width="70" height="90">
-                <img src="images/logo-kal.png" alt="Kalmar" width="80" height="90">
-            </div>
-
-            <div id="footer">
-                <img src="images/headbackground.jpg" alt="Red board" width=100% height="20">
-            </div>
-        </div>
+        <?php
+            include('sidfot.html');
+        ?>
 
     </body>
 </html>
