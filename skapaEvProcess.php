3db61994c401d099dd5e039ca3cf637bbb9774b9
Marina Bengtsson <bengtsson.marina@hotmail.com>
2018-05-21 13:06:23 +0200

    Pillat litt

--- skapaEvProcess.php
+++ skapaEvProcess.php
@@ -15,10 +15,29 @@ $evTyp = mysqli_real_escape_string($conn, trim($_POST['cars']));
 $tidFran = mysqli_real_escape_string($conn, trim($_POST['Från']));
 $tidTill = mysqli_real_escape_string($conn, trim($_POST['Till']));
 $evTid = mysqli_real_escape_string($conn, trim($_POST['Tid']));
+$evJa = ($_POST['Ja']);
+$evNej = ($_POST['Nej']);
+$evBeskrivning = mysqli_real_escape_string($conn, trim($_POST['Beskrivning']));
+
+if ((empty($evTitel)) || (empty($evTyp)) || (empty($tidFran)) || (empty($tidTill)) || (empty($evTid)) || (empty($evBeskrivning))){
+    echo 'OBS! Fyll i alla fält.';
+}
+else if ((!empty($evJa)) && (!empty($evNej)))
+{
+    echo 'OBS! Bara ett alternativ kan väljas.';
+}
+else if ((empty($evJa)) && (empty($evNej)))
+{
+    echo 'Ett alternativ måste väljas.';
+}
+
+else
+{
+    $sql = "INSERT INTO evenemang (title, typ, fran, till, tid, krav, beskrivning) VALUES ('$evTitel', '$evTyp', '$tidFran', '$tidTill', '$evTid', '$evJa', '$evNej', '$evBeskrivning')";
+}
 
 
 
-
-
+$conn->close();
 ?>
 
