8deeedc9cde9fa41bcd54593997f6c519726031d
Marina Bengtsson <bengtsson.marina@hotmail.com>
2018-05-21 11:45:26 +0200

    skapat en fil för skapaEv som lägger in skapade evenemang i databasen

--- skapaEvProcess.php
+++ skapaEvProcess.php
@@ -9,4 +9,16 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error){
     die("Connection failed: " . $conn->connect_error);
 }
+
+$evTitel = mysqli_real_escape_string($conn, trim($_POST['Titel']));
+$evTyp = mysqli_real_escape_string($conn, trim($_POST['cars']));
+$tidFran = mysqli_real_escape_string($conn, trim($_POST['Från']));
+$tidTill = mysqli_real_escape_string($conn, trim($_POST['Till']));
+$evTid = mysqli_real_escape_string($conn, trim($_POST['Tid']));
+
+
+
+
+
 ?>
+
