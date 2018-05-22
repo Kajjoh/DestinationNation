9c10e04032cb1748c8d00ea94e9d96077c495d77
Marina Bengtsson <bengtsson.marina@hotmail.com>
2018-05-22 14:17:23 +0200

    har gjort en fakesida för startsidan för screenshots.

--- registrering.php
+++ registrering.php
@@ -13,44 +13,46 @@
         ?>
 
         <!-- Här börjar innehållet. -->
-        <div id="rubrikRegistrering">
-            <h3> Registrera ett konto:</h3>
-        </div>
+        <div class = "helaEvenemang">
+            <div id="rubrikRegistrering">
+                <h3> Registrera ett konto:</h3>
+            </div>
 
-        <div id="registrationForm">
-            <form name="regForm" method="POST" action="regi-process.php" onsubmit="return (validateForm())">
+            <div id="registrationForm">
+                <form name="regForm" method="POST" action="regi-process.php" onsubmit="return (validateForm())">
 
-                <label for="namn">Personnummer</label><br>
-                    <input type="text" name="prnr" id="prnr"><br>
-        
-                <label for="namn">Student-id</label><br>
-                    <input type="text" name="stid" id="stid"><br>
+                    <label for="namn">Personnummer</label><br>
+                        <input type="text" name="prnr" id="prnr"><br>
+            
+                    <label for="namn">Student-id</label><br>
+                        <input type="text" name="stid" id="stid"><br>
 
-                <label for="namn">Namn</label><br>
-                    <input type="text" name="namn" id="namn"><br>
+                    <label for="namn">Namn</label><br>
+                        <input type="text" name="namn" id="namn"><br>
 
-                <label for="namn">E-mail</label><br>
-                    <input type="text" name="mail" id="mail"><br>
+                    <label for="namn">E-mail</label><br>
+                        <input type="text" name="mail" id="mail"><br>
 
-                <label for="namn">Adress</label><br>
-                    <input type="text" name="adress" id="adress"><br>
+                    <label for="namn">Adress</label><br>
+                        <input type="text" name="adress" id="adress"><br>
 
-                <label for="namn">Ort</label><br>
-                    <input type="text" name="ort" id="ort"><br>
+                    <label for="namn">Ort</label><br>
+                        <input type="text" name="ort" id="ort"><br>
 
-                <label for="namn">Postnummer</label><br>
-                    <input type="text" name="pstnr" id="pstnr"><br>
+                    <label for="namn">Postnummer</label><br>
+                        <input type="text" name="pstnr" id="pstnr"><br>
 
-                <label for="losen">Lösenord</label><br>
-                        <input type="password" placeholder="******" name="losen" id="losen"><br>
-				
-				<label for="upprepalosen">Upprepa lösenord</label><br>
-						<input type="password" placeholder="******" name="upprepalosen" id="upprepalosen"><br>
+                    <label for="losen">Lösenord</label><br>
+                            <input type="password" placeholder="******" name="losen" id="losen"><br>
+                    
+                    <label for="upprepalosen">Upprepa lösenord</label><br>
+                            <input type="password" placeholder="******" name="upprepalosen" id="upprepalosen"><br>
 
-                <input type="submit" value="Registrera!" id="submit">
+                    <input type="submit" value="Registrera!" id="submit">
 
-            </form>
-        
+                </form>
+            
+            </div>
         </div>
 
         <?php
--- startsidaStudent.php
+++ startsidaStudent.php
@@ -9,11 +9,14 @@
     
     <body>
 
+
     <?php
         include('html/sidhuvud.html');
 
         include('html/meny.html');
     ?>
+
+        
         
           <!--denna div (filter) listar horisontellt alla aktiviteter för nationer för filtrering.-->
         <div id="filter">
@@ -29,6 +32,48 @@
                 <li id="aktiviteter"><a id="attributAkt">Konsert</a></li>
                 <li id="aktiviteter"><a id="attributAkt">Övrigt</a></li>
             </ul>
+        </div>
+
+        <div class ="ssFlode">
+            <div class = "innerSSflode">
+                <h3 id = "ssRubrik">Evenemang</h3>
+
+                <div class = "event">
+                    <p>
+                        OCaml pluggstuga!
+                        <br>
+                        Tid: 18:00 - 22:00
+                        <br>
+                        Datum: 24/5 - 18
+                        <br>
+                        Plats: Östgöta nation
+                        <br>
+                        Välkomna till årets OCaml pluggstuga! 
+                        Vi kommer bjuda på goda hamburgare och dricka kommer kunna köpas
+                        i baren. Kalle Karlsson kommer gå igenom syntax, olika funktioner
+                        och kommer att kunna svara på frågor. 
+                        <br>
+                        Hoppas vi ses!
+                    </p>
+                </div>
+
+                <div class = "event">
+                    <p>
+                        Spring med GH!
+                        <br>
+                        Tid: 16:00 - 18:00
+                        <br>
+                        Datum: 16/4 - 18
+                        <br>
+                        Plats: Stadsskogen
+                        <br>
+                        Kom och spring med oss i Stadsskogen! Rundan är på 
+                        5 km och man springer helt i sin egen takt. Man får 
+                        frukt och vatten som diplom.
+                    </p>
+                </div>
+
+            </div>
         </div>
 
         <!--Denna div (nationsMeny) listar alla nationer vertikalt för filtrering.-->
@@ -47,6 +92,8 @@
             <a id="nationsattr" href="#">Västmanlands-Dala nation</a>
             <a id="nationsattr" href="#">Östgöta nation</a>
         </div>
+
+
 
         <?php
             include('html/sidfot.html');
