c432afb108d8ff664da203b47043fb5411fb0a72
kajsajohansson <kajssajohansson@gmail.com>
2018-05-23 15:02:04 +0200

    Ny förstasida

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>DestinationNation</title>
+		<link href="html/main.css" rel="stylesheet">
+		<meta charset="utf-8">
+    </head>
+	
+	<body>
+
+    <?php
+        include('html/sidhuvud.html');
+    ?>
+
+        <h2> Välkommen till DestinationNation!</h2>
+
+        <p class="student-nation" id="nationsKnapp"><a href="inloggningNation.php" >Logga in som Nation!</a></p>
+
+        <p class="student-nation" id="studentKnapp"><a href="inloggning.php" >Logga in som Student!</a></p>
+
+
+    <?php
+        include('html/sidfot.html');
+    ?>
+    	    	  
+	</body>
+</html>	
+	  
