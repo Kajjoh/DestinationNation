9820e3b7f66ac5e80bc6d2bf827b5039f7398011
Joel Flink <joelflink@gmail.com>
2018-05-24 17:05:59 +0200

    börjat lösa filtreringen

--- filtreraEvenemang.php
+++ filtreraEvenemang.php
@@ -6,11 +6,19 @@ $dbname = "projekt";
 
     // Create connection to database
     $conn = new mysqli($servername, $dbusername, $dbpassword, $dbname);
-
+	session_start();
     // Check connection with database
     if ($conn->connect_error) 
     {
         die("Connection failed: " . $conn->connect_error);
     }
 	
+
+	if (isset($_POST['pubKnapp']))
+	{
+		session_start();
+		$_SESSION['pub'] = 'pub';
+		header("Location: startsidaStudent.php");
+		
+	}
 	
