71b9c6c715e4d88350e401032bebc585c61b2d29
Joel Flink <joelflink@gmail.com>
2018-05-24 10:12:33 +0200

    Fixat validering och felmeddelande med sessions för registrering

--- registrering.php
+++ registrering.php
@@ -1,3 +1,6 @@
+<?php
+	session_start();
+?>
 <!DOCTYPE html>
 <html>
     <head>
@@ -23,31 +26,99 @@
                 <form name="regForm" method="POST" action="regi-process.php" onsubmit="return (validateForm())">
 
                     <label for="namn">Personnummer</label><br>
-                        <input type="text" name="prnr" id="prnr"><br>
+                        <input type="text" placeholder="XXXXXX-XXXX" name="prnr" id="prnr"><br>
+						<?php
+							if (isset($_SESSION['ogiltigtprnr']))
+							{
+								echo $_SESSION['ogiltigtprnr'];
+								unset($_SESSION['ogiltigtprnr']);
+							}
+						?><br>
             
                     <label for="namn">Student-id</label><br>
-                        <input type="text" name="stid" id="stid"><br>
+                        <input type="text" placeholder="XXXXXXXX" name="stid" id="stid"><br>
+						<?php
+							if (isset($_SESSION['ogiltigtstdid']))
+							{
+								echo $_SESSION['ogiltigtstdid'];
+								unset($_SESSION['ogiltigtstdid']);
+							}
+						?><br>
 
                     <label for="namn">Namn</label><br>
-                        <input type="text" name="namn" id="namn"><br>
+                        <input type="text" placeholder="Ditt namn..." name="namn" id="namn"><br>
+						<?php
+							if (isset($_SESSION['tomtnamn']))
+							{
+								echo $_SESSION['tomtnamn'];
+								unset($_SESSION['tomtnamn']);
+							}
+						?><br>
 
                     <label for="namn">E-mail</label><br>
-                        <input type="text" name="mail" id="mail"><br>
+                        <input type="text" placeholder="email@exempel.com" name="mail" id="mail"><br>
+						<?php
+							if (isset($_SESSION['emailmess']))
+							{
+								echo $_SESSION['emailmess'];
+								unset($_SESSION['emailmess']);
+							}
+							else if (isset($_SESSION['upptagenemail']))
+							{
+								echo $_SESSION['upptagenemail'];
+								unset($_SESSION['upptagenemail']);
+							}
+						?><br>
 
                     <label for="namn">Adress</label><br>
-                        <input type="text" name="adress" id="adress"><br>
+                        <input type="text" placeholder="Din adress..." name="adress" id="adress"><br>
+						<?php
+							if (isset($_SESSION['tomadress']))
+							{
+								echo $_SESSION['tomadress'];
+								unset($_SESSION['tomadress']);
+							}
+						?><br>
 
                     <label for="namn">Ort</label><br>
-                        <input type="text" name="ort" id="ort"><br>
+                        <input type="text" placeholder="Din ort..." name="ort" id="ort"><br>
+						<?php
+							if (isset($_SESSION['tomort']))
+							{
+								echo $_SESSION['tomort'];
+								unset($_SESSION['tomort']);
+							}
+						?><br>
 
                     <label for="namn">Postnummer</label><br>
-                        <input type="text" name="pstnr" id="pstnr"><br>
+                        <input type="text" placeholder="XXXXX" name="pstnr" id="pstnr"><br>
+						<?php
+							if (isset($_SESSION['ogiltigtpstnr']))
+							{
+								echo $_SESSION['ogiltigtpstnr'];
+								unset($_SESSION['ogiltigtpstnr']);
+							}
+						?><br>
 
                     <label for="losen">Lösenord</label><br>
                             <input type="password" placeholder="******" name="losen" id="losen"><br>
+							<?php
+							if (isset($_SESSION['tomtlosen']))
+							{
+								echo $_SESSION['tomtlosen'];
+								unset($_SESSION['tomtlosen']);
+							}
+						?><br>
                     
                     <label for="upprepalosen">Upprepa lösenord</label><br>
                             <input type="password" placeholder="******" name="upprepalosen" id="upprepalosen"><br>
+							<?php
+							if (isset($_SESSION['losenmatch']))
+							{
+								echo $_SESSION['losenmatch'];
+								unset($_SESSION['losenmatch']);
+							}
+						?><br>
 
                     <input type="submit" value="Registrera!" id="submit">
 
