9c872193a4895215d8fc2ab61e2f8078669813f0
Marina Bengtsson <bengtsson.marina@hotmail.com>
2018-05-24 14:08:07 +0200

    någon liten ändring

--- java/AnmalanEv.js
+++ java/AnmalanEv.js
@@ -4,17 +4,11 @@ Anmalan.addEventListener("submit", validateAnmalan);
 function validateAnmalan (event)
 {
 	var Namn = document.AnmalanEv.Namn.value;
-	var Mail = document.AnmalanEv.Mail.value;
-	var Sallskap = document.AnmalanEv.Sallskap.value;
-	var Allergi = document.AnmalanEv.Allergi.value;
 
-
-	if(Namn == "" || (Mail == "")) /* Skapar en if-sats och om det är tomma fält skickas en alert ut med information*/
+	if(Namn == "") /* Skapar en if-sats och om det är tomma fält skickas en alert ut med information*/
 	{
 		event.preventDefault(); /* om det i if-satsen inte stämmer skickas aldrig data, 
 			man preventar default vilket är att skicka data*/
 		alert('Du måste fylla i namn för att kunna anmäla dig till ett evenemang');
 	}
-}
-
 }
