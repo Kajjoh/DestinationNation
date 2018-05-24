5ef9f499e5512a14babce28a259e3d6b5cc04582
Mattias <mattias.ekwall@hotmail.se>
2018-05-24 09:27:28 +0200

    exempelkommentar lades till

--- javaMeny.js
+++ javaMeny.js
@@ -6,3 +6,7 @@ function validateForm () /*första funktionen*/
 		var Loggaut = document.Meny.LoggaUt.value.trim(); /*Hämtar det värdet som skrivs in i fältet och trimmar det som sedan läggs in i pName*/
 		console.log(LoggaUt);/*ett sätt att se i consolen vad som skrivs in i olika fält*/
 	}
+
+
+	//skapar nationsid i nationer och som FK  i evenemang och matchar på det
+	$value1 = "SELECT * FROM evenemang WHERE nationsID = 6"
