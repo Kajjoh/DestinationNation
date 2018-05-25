d08c18b578b7e491b10e73f0b54fe4fdc9c8e08b
Mattias <mattias.ekwall@hotmail.se>
2018-05-25 17:20:57 +0200

    fixat så man kan publicera meddelanden

--- publiceraTjanst.php
+++ publiceraTjanst.php
@@ -11,5 +11,56 @@
 			include('html/sidhuvud.html');
 			include ('html/menyMedlemmar.html');
 		?>
+		<div class="formular">
+			<form id ="publicera" name="publicera" method="POST" action="process/publiceraTjanst-process.php" onsubmit="return (validateEv())">
+
+					<label for="Titel">Titel</label><br>
+						<input type="text" id="Titel" name="Titel"><br>
+						<div class="errorMessage">
+							<?php
+							if (isset($_SESSION['felmeddelande']))
+							{
+								echo $_SESSION['felmeddelande'];
+								unset ($_SESSION['felmeddelande']);
+							}
+							?>
+                    	</div><br>
+                    	<label for="Tid">Tid</label><br>
+						<input type="text" id="Tid" name="Tid"><br>
+						<div class="errorMessage">
+							<?php
+							if (isset($_SESSION['felmeddelande']))
+							{
+								echo $_SESSION['felmeddelande'];
+								unset ($_SESSION['felmeddelande']);
+							}
+							?>
+                    	</div><br>
+                    	<label for="Datum">Datum</label><br>
+						<input type="text" id="Datum" name="Datum"><br>
+						<div class="errorMessage">
+							<?php
+							if (isset($_SESSION['felmeddelande']))
+							{
+								echo $_SESSION['felmeddelande'];
+								unset ($_SESSION['felmeddelande']);
+							}
+							?>
+                    	</div><br>
+                    	<label for="Meddelande">Meddelande</label><br>
+						<textarea id="Meddelande" name="Meddelande" placeholder="Publicering av meddelande"></textarea><br>
+						<div class="errorMessage">
+							<?php
+							if (isset($_SESSION['felmeddelande']))
+							{
+								echo $_SESSION['felmeddelande'];
+								unset ($_SESSION['felmeddelande']);
+							}
+							?>
+                    	</div><br>
+
+                    	<input type="submit" value="Publicera meddelande!" id="publiceraBtn">
+            </form>
+		</div>
 	</body>
 </html>
