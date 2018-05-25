84fd02a6d6680de21d98754aa9a2dcfe3e00d8b2
Marina Bengtsson <bengtsson.marina@hotmail.com>
2018-05-25 11:36:36 +0200

    fixat till sql-satsen så den tar med allt från evenemangs tabellen och fixat en scroll på minasidor nation

--- process/mSN-process.php
+++ process/mSN-process.php
@@ -12,20 +12,26 @@ $dbname = "projekt";
     {
         die("Connection failed: " . $conn->connect_error);
     } 
+
 	$sql = "SELECT * FROM evenemang";
     $result = $conn->query($sql);
 
-    $row=mysqli_fetch_assoc($result);
-    echo "<div> <br> Titel: ". $row["titel"]."
-                <br> Plats: ". $row["plats"]." 
-                <br> Nation: ".$row["nation"]."
-                <br> Typ: " . $row["typ"]. "
-                <br> Från: " .$row["fran"]."
-                <br> Till: " .$row["till"]."
-                <br> Datum: " .$row["datum"]."
-                <br> Kräver anmälan: " .$row["krav"]."
-                <br> Beskrivning: " .$row["beskrivning"]."       
-            </div>";
+    if($result->num_rows > 0)
+    {
+        while($row=$result->fetch_assoc())
+        {
+            echo "<div> <br> Titel: ". $row["titel"]."
+            <br> Plats: ". $row["plats"]." 
+            <br> Nation: ".$row["nation"]."
+            <br> Typ: " . $row["typ"]. "
+            <br> Från: " .$row["fran"]."
+            <br> Till: " .$row["till"]."
+            <br> Datum: " .$row["datum"]."
+            <br> Kräver anmälan: " .$row["krav"]."
+            <br> Beskrivning: " .$row["beskrivning"]."       
+        </div>";
+        }
+    }
      
     $conn->close();
 ?>
