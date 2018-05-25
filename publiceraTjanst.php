2b87cbdb8ec9fdf77d20e3a33be4badee01cd985
Mattias <mattias.ekwall@hotmail.se>
2018-05-25 16:39:07 +0200

    publiceringsformular

---
+++ publiceraTjanst.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>DestinationNation</title>
+		<link href="html/main.css" rel="stylesheet">
+		<meta charset="utf-8">
+    </head>	
+	
+	<body>
+		<?php
+			include('html/sidhuvud.html');
+			include ('html/menyLeverantor.html')
+		?>
+	</body>
+</html>
