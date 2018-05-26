83c02af5cfaef2441afbaffe23c648745d34fc5b
Mattias <mattias.ekwall@hotmail.se>
2018-05-26 09:58:18 +0200

    gjort javascript för publicera

---
+++ java/publicera.js
@@ -0,0 +1,9 @@
+var publicera = document.getElementById('publicera');
+publicera.addEventListener("submit", validatepub);
+
+function validatepub(event)
+{
+	var Titel = document.publicera.Titel.value.trim();
+	var Tid = docuent.publicera.Titel.value.trim();
+	
+}
