e0fc49e88a51f6a4d62e45e297c533174fc93848
Mattias <mattias.ekwall@hotmail.se>
2018-05-26 10:56:28 +0200

    fixat felmeddelanden med alert

--- java/publicera.js
+++ java/publicera.js
@@ -4,6 +4,17 @@ publicera.addEventListener("submit", validatepub);
 function validatepub(event)
 {
 	var Titel = document.publicera.Titel.value.trim();
-	var Tid = docuent.publicera.Titel.value.trim();
-	
+	var Tid = document.publicera.Titel.value.trim();
+	var Datum = document.publicera.Datum.value.trim();
+	var Meddelande = document.publicera.Meddelande.value.trim();
+
+	if(Titel == "" || (Tid == "") || (Datum == "") || (Meddelande==""))
+	{
+		event.preventDefault();
+		alert('Vänligen fyll i alla fält för publicera ett meddelande');
+	}
+	else
+	{
+		alert('Ett meddelande är nu skapat');
+	}
 }
