bee0e09ea650a3819f63ea070fdb1ca0d83cdf60
Mattias <mattias.ekwall@hotmail.se>
2018-05-27 11:14:33 +0200

    fixat felmeddelande med sessions

--- process/publiceraTjanst-process.php
+++ process/publiceraTjanst-process.php
@@ -12,33 +12,35 @@
     $timeMatch = "/^([0-9]|0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]$/";
     $dateMatch = "/([12]\d{3}-(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01]))/";
     $tjanst = $_SESSION['admin'];
+    $_SESSION['felmeddelande'] = "Du måste fylla i alla fält!";
+    $felmeddelande = $_SESSION['felmeddelande'];
 
     if ($pubTitel=="")
     {
-        $_SESSION['felmeddelande'] = "Du måste fylla i alla fält!";
+         $felmeddelande;
         header("Location: ../publiceraTjanst.php");
     }
     else if ($pubTid=="")
     {
-        $_SESSION['felmeddelande'] = "Du måste fylla i alla fält!";
+        $felmeddelande;
         header("Location: ../publiceraTjanst.php");
 
     }
     else if ($pubDatum=="")
     {
-        $_SESSION['felmeddelande'] = "Du måste fylla i alla fält!";
+        $felmeddelande;
         header("Location: ../publiceraTjanst.php");
     }
     else if ($pubMeddelande=="") 
     {
-        $_SESSION['felmeddelande'] = "Du måste fylla i alla fält!";
+        $felmeddelande;
         header("Location: ../publiceraTjanst.php");
     }
-   else if (!(preg_match($timeMatch, $pubtid)))
+   else if (!(preg_match($timeMatch, $pubTid)))
     {
         $_SESSION['felTid'] = "Tiden ska skrivas som 00:00.";
         header("Location: ../publiceraTjanst.php");
-    }    
+    } 
     else if (!(preg_match($dateMatch, $pubDatum)))
     {
         $_SESSION['felDatum'] = "Datumet ska skrivas som 2018-01-01.";
