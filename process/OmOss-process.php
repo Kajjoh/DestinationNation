ac9216fe2c861ca2f75375fd1bc1d11356c3708f
Mattias <mattias.ekwall@hotmail.se>
2018-05-27 10:02:17 +0200

    fixat div kring processen så att meddelanden ka nstylas i css

--- process/OmOss-process.php
+++ process/OmOss-process.php
@@ -1,26 +1,36 @@
-<?php
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>DestinationNation</title>
+        <link href="html/main.css" rel="stylesheet">
+        <meta charset="utf-8">
+    </head>
+    <div id="display">
+        <?php
 
-include('connection.php');
+            include('connection.php');
 
-    $sql = "SELECT  Titel, Tid, Datum, Meddelande FROM meddelande";
-    $result = $conn->query($sql);
+            $sql = "SELECT  Titel, Tid, Datum, Meddelande FROM meddelande";
+            $result = $conn->query($sql);
 
-    if($result->num_rows > 0)
-    {
-        while($row=$result->fetch_assoc())
-        {
-            echo "<div> 
-                    <br> Titel: ". $row["Titel"]."
-                    <br> Tid: ".$row["Tid"]."
-                    <br> Datum: ". $row["Datum"]."
-                    <br> Meddelande: ". $row["Meddelande"]."                  
-                </div>";
-        }
-    }      
-    else
-    {
-        echo "Det finns inget meddelande att visa!";
-    }
+            if($result->num_rows > 0)
+            {
+                while($row=$result->fetch_assoc())
+                {
+                    echo "<div> 
+                            <br> Titel: ". $row["Titel"]."
+                            <br> Tid: ".$row["Tid"]."
+                            <br> Datum: ". $row["Datum"]."
+                            <br> Meddelande: ". $row["Meddelande"]."                  
+                        </div>";
+                }
+            }      
+            else
+            {
+                echo "Det finns inget meddelande att visa!";
+            }
 
-    $conn->close();
-?>
+             $conn->close();
+        ?>
+    </div>
+</html>
