c26a525f3ca4fe7c0cee4b3d7472da9729a404a0
Joel Flink <joelflink@gmail.com>
2018-05-29 10:06:36 +0200

    nu får man utan meddelande om man redan sparat ett evenemang

--- sparaEvenemang.php
+++ sparaEvenemang.php
@@ -14,38 +14,45 @@
 
         include('html/menyTillbakaStudent.html');
     ?>
-				
-		<h2>Spara Evenemang</h2>
-			<div class="midForm">			
+	
+		<div class="formular" id="sparaEv">
+	
 			
+			<h2>Spara Evenemang</h2>
+			<div id="sparaevform">			
 			<!-- Här börjar formuläret. -->
-				<form name="sparaevenemang" method="POST" action="process/sparaev-process.php"><br>
+				<form name="sparaevenemang" method="POST" action="process/sparaev-process.php">
+		
 				
-
 					<label for="evenemang">Evenemangs ID:<br></label>
-						<input type="text" placeholder="..." name="sparaEvenemang" id="sparaEvenemang" class="boxes"><br><br>
-					
-						<div class="errorMessage">
-							<?php
-								session_start();
-								if (isset($_SESSION['tomtevid']))
-								{
-									echo $_SESSION['tomtevid'];
-									unset($_SESSION['tomtevid']);
-								}
-								
-								else if (isset($_SESSION['idfinnsej']))
-								{
-									echo $_SESSION['idfinnsej'];
-									unset($_SESSION['idfinnsej']);
-								}
-							?><br>
+					<input type="text" placeholder="..." name="sparaEvenemang" id="sparaEvenemang"><br>
+					<div class="errorMessage">
+						<?php
+							session_start();
+							if (isset($_SESSION['tomtevid']))
+							{
+								echo $_SESSION['tomtevid'];
+								unset($_SESSION['tomtevid']);
+							}
+							
+							else if (isset($_SESSION['idfinnsej']))
+							{
+								echo $_SESSION['idfinnsej'];
+								unset($_SESSION['idfinnsej']);
+							}
+							else if (isset($_SESSION['redansparat']))
+							{
+								echo $_SESSION['redansparat'];
+								unset($_SESSION['redansparat']);
+							}
+							
+						?>
 					</div>					
 					<input type="submit" value="Spara evenemang" id="sparEv" class="button">
 				
 				</form>
 			</div>
-
+		</div>
 	<?php
 		include('html/sidfot.html');
 	?>	
